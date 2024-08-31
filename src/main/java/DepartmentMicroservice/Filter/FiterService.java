b18d6056621afbe644b28bf82ac8f7a49ecc1295
Bhatt, Sagar <SagarBhatt@geico.com>
2024-08-31 14:17:22 -0600

    Implemented the filtering and added the custom header from Filter interfacs's filterChain.doFiltter()

---
+++ src/main/java/DepartmentMicroservice/Filter/FiterService.java
@@ -0,0 +1,18 @@
+package DepartmentMicroservice.Filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class FiterService implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        resp.addHeader("Custom-header","Filter-custom-resp");
+        filterChain.doFilter(servletRequest,resp);
+    }
+}
